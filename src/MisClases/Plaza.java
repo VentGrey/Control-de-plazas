c745d5286fb412ad25e4af6b966598c152d46718
VentGrey <omarpurataf@gmail.com>
2018-11-24 16:50:00 -0600

    Se agregó la clase base para la plaza

---
+++ src/MisClases/Plaza.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MisClases;
+
+/**
+ *
+ * @author omar
+ */
+public class Plaza {
+    String Nombre;
+    Integer Policias;
+    Integer NPC;
+    String Ciudad;
+    String IdPlaza; //Único
+    
+}
