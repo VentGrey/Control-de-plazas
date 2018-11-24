50a084b930b8e44718152d547edc86b59a4d58b6
VentGrey <omarpurataf@gmail.com>
2018-11-24 16:36:00 -0600

    Se agregó la clase base para comandantes

---
+++ src/MisClases/Comandante.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MisClases;
+
+/**
+ *
+ * @author omar
+ */
+public class Comandante {
+    
+    String Nombre, Apaterno, Especialidad;
+    Integer Cedula, Antiguedad;
+    
+    public Comandante(String Nombre, String Apaterno, String Especialidad ) {
+    this.Nombre = Nombre;
+    this.Apaterno = Apaterno;
+    this.Especialidad = Especialidad;
+}
+    
+}
